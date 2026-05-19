a5aa2e6b16b5b9ce5a76c63a185123ba6a63439f
hasini2727 <shasini2727@gmail.com>
2026-05-19 23:49:31 +0530

    feat: Two Sum - HashMap complement tracking - O(n)

--- ERROR_LOG.md
+++ ERROR_LOG.md
@@ -1,2 +1,16 @@
 # DSA Error Log
    Tracking bugs, logic slip-ups, and key takeaways here.
+
+## Date: May 19, 2026
+**Problem:** Two Sum
+
+**Why I got stuck:** 
+- Brain defaulted to nested loop
+- Mixed up Keys and Values
+
+**What I learned:** 
+- Key = Data to find instantly (complement number: target - currentNum).
+- Value = Data to retrieve (the index of that old number).
+- Hash Map lets you look backward in O(1) 
+
+**Pattern Missed:** Hash Map Lookback / Complement Search
---
+++ TwoSum.java
@@ -0,0 +1,19 @@
+//Problem: Two sum
+//Pattern: Hash Map
+//Time: O(1) | Space:O(n)
+//Signal: Searching for a pair that meets a target sum? Try calculating the complement (target - current) and look backward using a Map.
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++){
+            int currentNum = nums[i];
+            int requiredNum = target - currentNum;
+            if(map.containsKey(requiredNum)){
+                return new int [] {map.get(requiredNum), i};
+            }
+            map.put(currentNum, i);
+        }
+        return new int [] {};
+    }
+}
